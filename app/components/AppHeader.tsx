d9c3e16ccb198c7dd2bcbf105ab36d89ba143e07
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-12-11 22:47:36 +0530

    re-factor: Update AppHeader

--- app/components/AppHeader.tsx
+++ app/components/AppHeader.tsx
@@ -1,22 +1,21 @@
 import { PropsWithChildren, ReactElement } from "react";
 import type { User } from '@supabase/supabase-js'
-import { Form } from 'remix'
-
-import Button from './Button'
+import { Form, useTransition } from 'remix'
 
 type AppHeaderProps = {
     user?: User
 }
 
 function AppHeader({ user }: PropsWithChildren<AppHeaderProps>): ReactElement {
+    const transition = useTransition()
     return (
         <nav className="w-full py-3 bg-blue-50 shadow-md ">
             <div className="container flex justify-end place-content-end">
                 <ul className="list-none flex gap-4 text-center">
                     <li className="flex gap-2">
-                        { user?.email } &nbsp;
+                    {transition.state === 'submitting' ?? <em>signing out, </em>}{ user?.email }{transition.state === 'submitting' ?? <em>...</em>} &nbsp;
                         <Form method="post" action="/signout">
-                            <Button type="submit" size='xs' variant="secondary">Sign Out</Button>
+                            <button type="submit" className="px-4 py-1 rounded-md bg-cyan-500 text-white shadow-lg shadow-cyan-500/50" disabled={transition.state === 'submitting'}>Sign Out</button>
                         </Form>
                     </li>
                 </ul>
